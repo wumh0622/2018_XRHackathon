fdc28baca8ab3871bb1db00f58be7c0efa491c1c
吳孟宏 <wueric0622@gmail.com>
2018-07-27 15:16:36 +0800

    Guest

---
+++ Assets/Script/Guest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guest : MonoBehaviour {
+    //[SerializeField] List<>
+
+    public NavMeshAgent nav;
+    public enum myAction
+    {
+        None,
+		Request,
+		Talk
+    }
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(Input.GetKeyDown("s"))
+            GuestMove(Vector3.zero);
+		if(Input.GetKeyDown("a"))
+            GuestAction(myAction.Talk);
+    }
+    public void GuestMove(Vector3 _pos)
+	{
+		if(nav == null)
+		{
+            Debug.LogWarning("NoNavMesh");
+            return;
+        }
+        nav.SetDestination(_pos);
+    }
+
+	public void GuestAction(myAction _action)
+	{
+		switch (_action)
+		{
+			case myAction.Request:
+			{
+                    Debug.Log("I need ");
+            }break;
+			case myAction.Talk:
+			{
+                    Debug.Log("I Just Talk ");
+            }break;
+            default:
+                break;
+        }
+	}
+
+	public void CompleteGuest(bool _Iscomplete)
+	{
+		if(_Iscomplete)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+}
---
+++ Assets/Script/GuestManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestManager : MonoBehaviour {
+
+	public static GuestManager instance;
+/// <summary>
+/// Awake is called when the script instance is being loaded.
+/// </summary>
+	void Awake()
+	{
+		
+	}
+	public void InstantiateGuest(){
+		
+	}
+}
