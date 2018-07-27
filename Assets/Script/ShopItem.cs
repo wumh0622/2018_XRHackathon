d195b5345ad064b53f52f5feb69bb84aac8f727d
WuMH <wueric0622@gmail.com>
2018-07-27 15:22:44 +0800

    新增控制器功能 物件互動

---
+++ Assets/Script/ShopItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.Highlighters;
+[RequireComponent(typeof(VRTK_OutlineObjectCopyHighlighter))]
+public class ShopItem : VRTK.VRTK_InteractableObject
+{
+
+    // Update is called once per frame
+
+	public override void StartUsing(VRTK_InteractUse currentUsingObject)
+	{
+        base.StartUsing(currentUsingObject);
+        Debug.Log("Use!!!");
+	}
+
+}
